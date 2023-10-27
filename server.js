0fcfc5dac7df7ba27820a51892816f93a7b34e78
Mir_Maks13 <matskiv2005@gmail.com>
2023-10-27 11:18:36 +0300

    Cleaned some unused code

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const { MongoClient } = require('mongodb');
 const path = require('path');
 const fs = require('fs');
 const mongoose = require('mongoose');
-//var app = express();
 
 const app = express();
 const port = 3000;
@@ -40,13 +39,13 @@ db.once('open', async () => {
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
-// GET-запит для відображення форми завантаження файлів
-app.get('/', (req, res) => {
+
+app.get('/', (req, res) => { // GET-запит для відображення форми завантаження файлів
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// GET-запит для відображення GIF
-app.get('/get-gif/:id', async (req, res) => {
+
+app.get('/get-gif/:id', async (req, res) => { // GET-запит для відображення GIF
 try {
   const gifId = req.params.id;
   const gif = await GifModel.findById(gifId);
@@ -55,8 +54,8 @@ try {
     return res.status(404).send('GIF not found');
   }
 
-  // Встановлення правильного Content-Type для GIF
-  res.setHeader('Content-Type', gif.contentType);
+
+  res.setHeader('Content-Type', gif.contentType);   // Встановлення правильного Content-Type для GIF
   res.send(gif.data);
 } catch (error) {
   res.status(500).send('Помилка відкриття GIF з бази даних');
@@ -71,15 +70,15 @@ app.get('/get-gif-text/:id', async (req, res) => {
       return res.status(404).send('GIF not found');
     }
 
-    // Встановіть правильний Content-Type для текстового файлу
-    res.setHeader('Content-Type', 'text/plain');
+
+    res.setHeader('Content-Type', 'text/plain');     // Встановлення правильного Content-Type для текстового файлу
     res.setHeader('Content-Disposition', 'attachment; filename=gif.txt');
     
-    // Конвертуйте бінарні дані GIF у формат Base64
-    const base64Data = gif.data.toString('base64');
+  
+    const base64Data = gif.data.toString('base64');   // Конвертування бінарних даних GIF у формат Base64
     
-    // Відправте дані у формі текстового файлу
-    res.send(base64Data);
+
+    res.send(base64Data);     // Відправка даних у формі текстового файлу
   } catch (error) {
     res.status(500).send('Помилка відкриття GIF з бази даних');
   }
@@ -110,8 +109,8 @@ app.get('/download-gif/:id', async (req, res) => {
       return res.status(404).send('GIF not found');
     }
 
-    // Встановіть правильний Content-Type для GIF
-    res.setHeader('Content-Type', gif.contentType);
+ 
+    res.setHeader('Content-Type', gif.contentType);    // Встановлення правильного Content-Type для GIF
     res.setHeader('Content-Disposition', 'attachment; filename=downloaded.gif');
     res.send(gif.data);
   } catch (error) {
@@ -128,8 +127,8 @@ app.get('/save-gif/:id', async (req, res) => {
       return res.status(404).send('GIF not found');
     }
 
-    // Створіть файл та записуйте в нього бінарні дані
-    const filePath = `uploads/${gifId}.gif`;
+
+    const filePath = `uploads/${gifId}.gif`;     // Створення файлу та записування в нього бінарних даних
     fs.writeFileSync(filePath, gif.data);
 
     res.download(filePath); // Відправте файл користувачеві для завантаження
@@ -139,15 +138,13 @@ app.get('/save-gif/:id', async (req, res) => {
 });
 
 
-// POST-запит для завантаження файлів
-app.post('/upload', upload.single('file'), async (req, res) => {
+
+app.post('/upload', upload.single('file'), async (req, res) => { // POST-запит для завантаження файлів
   try {
     if (req.file) {
       const { originalname, buffer, mimetype } = req.file;
 
-
-      // Зчитати файл як бінарні дані
-      const fileData = fs.readFileSync(req.file.path);
+      const fileData = fs.readFileSync(req.file.path); // Зчитати файл як бінарні дані
       
       const gif = new GifModel({
         filename: originalname,
@@ -165,30 +162,6 @@ app.post('/upload', upload.single('file'), async (req, res) => {
     res.status(500).send('Помилка збереження файлу в базу даних.');
   }
 });
-
-/*app.post('/convert-text-to-gif', async (req, res) => {
-  try {
-    const { text } = req.body; // Отримайте текст з тіла запиту
-
-    if (!text) {
-      return res.status(400).send('Text not provided in the request body.');
-    }
-
-    // Створіть буфер із тексту у формат Base64
-    const binaryData = Buffer.from(text, 'base64');
-
-    if (!binaryData) {
-      return res.status(400).send('Failed to convert text to binary data.');
-    }
-
-    // Відправте бінарні дані як GIF відповідь на запит
-    res.setHeader('Content-Type', 'image/gif');
-    res.send(binaryData);
-  } catch (error) {
-    console.error('Error converting text to GIF:', error);
-    res.status(500).send('Помилка конвертації тексту в GIF.');
-  }
-});*/
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
