e6c349be8792e42c506f841b835069408066de1a
Maxym Matskiv <maksymvolodymyr.matskiv@lnu.edu.ua>
2024-11-20 21:05:39 +0200

    fix: Update setup script to use correct paths for requirements and manage.py

--- python_server/setup.sh
+++ python_server/setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 pip install setuptools
-pip install -r requirements.txt
+pip install -r python_server/requirements.txt
 
-python manage.py makemigrations
-python manage.py migrate
-python manage.py tailwind install
-python manage.py collectstatic --noinput
-python manage.py tailwind start
+python python_server/manage.py makemigrations
+python python_server/manage.py migrate
+python python_server/manage.py tailwind install
+python python_server/manage.py collectstatic --noinput
+python python_server/manage.py tailwind start
